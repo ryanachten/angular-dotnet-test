1ab3b7a57026680c059f45b31d95e3de2ba5ea7e
Ryan Achten <ryanachten@gmail.com>
2019-09-10 17:23:37 +1200

    Add API CORS policy

--- AngularDotnet.API/Startup.cs
+++ AngularDotnet.API/Startup.cs
@@ -29,6 +29,7 @@ namespace AngularDotnet.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +46,9 @@ namespace AngularDotnet.API
             }
 
             // app.UseHttpsRedirection(); // Enforces HTTPS - uncomment later
+
+            // API CORS policy
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()); //TODO: change AllowAnyOrigin to a more specific address
             app.UseMvc();
         }
     }
