6cede24b6bd08e7b88699f877db9d519ed86a838
Ryan Achten <ryanachten@gmail.com>
2019-09-25 08:32:46 +1200

    Add Register method to AuthRespository

--- AngularDotnet.API/Data/AuthRepository.cs
+++ AngularDotnet.API/Data/AuthRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using AngularDotnet.API.Models;
 
@@ -18,9 +19,33 @@ namespace AngularDotnet.API.Data
             throw new System.NotImplementedException();
         }
 
-        public Task<User> Register(User user, string password)
+        public async Task<User> Register(User user, string password)
         {
-            throw new System.NotImplementedException();
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            // Save the user to the database
+            await _context.Users.AddAsync(user);
+            // Save database changes
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                // Randomly generated key
+                passwordSalt = hmac.Key;
+
+                // Convert the password to a byte array so that is can be used to produce the hash
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+            throw new NotImplementedException();
         }
 
         public Task<bool> UserExists(string username)
