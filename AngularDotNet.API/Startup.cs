53b3a573f3481d6639ca796c42133ded155db831
Ryan Achten <ryan.achten@akqa.com>
2020-03-19 19:05:02 +1300

    Configure API to enable CORS

--- AngularDotNet.API/Startup.cs
+++ AngularDotNet.API/Startup.cs
@@ -29,6 +29,7 @@ namespace AngularDotNet.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +45,9 @@ namespace AngularDotNet.API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            // This could be more specific if security needs to be stronger
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseAuthorization();
 
