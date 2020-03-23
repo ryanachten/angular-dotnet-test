ae648cd5ca2bc37790eef1618082b4b2feed0427
Ryan Achten <ryan.achten@akqa.com>
2020-03-24 08:03:17 +1300

    Add validation for UserForRegisterDto

--- AngularDotNet.API/DTOs/UserForRegisterDto.cs
+++ AngularDotNet.API/DTOs/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AngularDotnet.API.DTOs
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(10, MinimumLength = 4, ErrorMessage = "Password must be between 4 and 10 characters")]
         public string Password { get; set; }
     }
 }
