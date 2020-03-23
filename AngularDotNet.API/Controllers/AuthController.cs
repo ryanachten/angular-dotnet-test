ceba6fae13d7f000ceceac3cf0dbccf206bf7e03
Ryan Achten <ryan.achten@akqa.com>
2020-03-23 19:17:41 +1300

    Add register data transfer object

--- AngularDotNet.API/Controllers/AuthController.cs
+++ AngularDotNet.API/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using AngularDotnet.API.Data;
+using AngularDotnet.API.DTOs;
 using AngularDotnet.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,19 +17,19 @@ namespace AngularDotnet.API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(string username, string password)
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
             // TODO: validate request
-            username = username.ToLower();
-            if (await _repo.UserExists(username))
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+            if (await _repo.UserExists(userForRegisterDto.Username))
                 return BadRequest("User name already exists");
 
             var userToCreate = new User
             {
-                Username = username
+                Username = userForRegisterDto.Username
             };
 
-            var createdUser = await _repo.Register(userToCreate, password);
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
             // TODO: should be using CreatedAtRoute
             return StatusCode(201);
---
+++ AngularDotNet.API/DTOs/UserForRegisterDto.cs
@@ -0,0 +1,8 @@
+namespace AngularDotnet.API.DTOs
+{
+    public class UserForRegisterDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
