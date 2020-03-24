63a3fa5977d7e78109f62b8baa0762c099aae523
Ryan Achten <ryan.achten@akqa.com>
2020-03-25 08:23:30 +1300

    Add login method and token handler logic

--- AngularDotNet.API/Controllers/AuthController.cs
+++ AngularDotNet.API/Controllers/AuthController.cs
@@ -1,8 +1,12 @@
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using AngularDotnet.API.Data;
 using AngularDotnet.API.DTOs;
 using AngularDotnet.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AngularDotnet.API.Controllers
 {
@@ -11,15 +15,16 @@ namespace AngularDotnet.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
+            _config = config;
             _repo = repo;
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
-            // TODO: validate request
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
             if (await _repo.UserExists(userForRegisterDto.Username))
                 return BadRequest("User name already exists");
@@ -31,6 +36,24 @@ namespace AngularDotnet.API.Controllers
 
             var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
+            // TODO: should be using CreatedAtRoute
+            return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.Username, userForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new[]{
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes());
             // TODO: should be using CreatedAtRoute
             return StatusCode(201);
         }
