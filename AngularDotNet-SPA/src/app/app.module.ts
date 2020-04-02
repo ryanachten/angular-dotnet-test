516b630f3a3b4892b4ce3450f38bee2d7918af8c
Ryan Achten <ryan.achten@akqa.com>
2020-04-03 07:44:00 +1300

    Add ngx bootstrap to nav dropdown

--- AngularDotNet-SPA/src/app/app.module.ts
+++ AngularDotNet-SPA/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BsDropdownModule } from "ngx-bootstrap/dropdown";
+
 import { AppComponent } from "./components/app.component";
 import { NavComponent } from "./components/nav/nav.component";
 import { AuthService } from "./services/auth.service";
@@ -12,7 +15,13 @@ import { ErrorInterceptorProvider } from "./services/error.interceptor";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NavComponent, RegisterComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
+  ],
   providers: [AuthService, ErrorInterceptorProvider],
   bootstrap: [AppComponent]
 })
