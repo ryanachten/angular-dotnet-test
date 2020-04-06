158b5f85b1fb2a186e16f6984a9a43baa14c475a
Ryan Achten <ryan.achten@akqa.com>
2020-04-07 07:51:42 +1200

    Add city to User class

--- AngularDotNet.API/Models/User.cs
+++ AngularDotNet.API/Models/User.cs
@@ -17,6 +17,7 @@ namespace AngularDotnet.API.Models
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
         public string Interests { get; set; }
+        public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
     }
