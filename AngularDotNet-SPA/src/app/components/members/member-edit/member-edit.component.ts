66cfdac0a2bcc3e22f15e236f72c3ad6487026dc
Ryan Achten <ryan.achten@akqa.com>
2020-04-15 07:32:28 +1200

    Add edit member form logic and styles

--- AngularDotNet-SPA/src/app/components/members/member-edit/member-edit.component.ts
+++ AngularDotNet-SPA/src/app/components/members/member-edit/member-edit.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { User } from "src/app/models/user";
+import { AlertService } from "src/app/services/alert.service";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: "app-member-edit",
   templateUrl: "./member-edit.component.html",
-  styleUrls: ["./member-edit.component.css"],
+  styleUrls: ["./member-edit.component.scss"],
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild("editForm", { static: true }) editForm: NgForm;
   user: User;
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private alert: AlertService) {}
 
   ngOnInit() {
     this.route.data.subscribe((data) => {
       this.user = data["user"];
     });
+  }
+
+  updateUser() {
+    console.log(this.user);
+    this.alert.success("Profile updated");
+    this.editForm.reset(this.user);
   }
 }
