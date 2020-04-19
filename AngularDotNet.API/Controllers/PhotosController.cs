ad7ce88fec1a0a941ac03dfcc98ad6f944daf8ac
Ryan Achten <ryan.achten@akqa.com>
2020-04-19 16:31:57 +1200

    Add init PhotosController and PhotoForCreationDto

---
+++ AngularDotNet.API/Controllers/PhotosController.cs
@@ -0,0 +1,86 @@
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AngularDotnet.API.Data;
+using AngularDotnet.API.DTOs;
+using AngularDotnet.API.Helpers;
+using AngularDotnet.API.Models;
+using AutoMapper;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace AngularDotnet.API.Controllers
+{
+    [Authorize]
+    [Route("api/users/{userId}/photos")]
+    [ApiController]
+    public class PhotosController : ControllerBase
+    {
+        private readonly IDatingRepository _repo;
+        private readonly IMapper _mapper;
+        private readonly IOptions<CloudinarySettings> _cloudinaryConfig;
+        private readonly Cloudinary _cloudinary;
+
+        public PhotosController(IDatingRepository repo, IMapper mapper, IOptions<CloudinarySettings> cloudinaryConfig)
+        {
+            _cloudinaryConfig = cloudinaryConfig;
+            _mapper = mapper;
+            _repo = repo;
+
+            Account acc = new Account(
+                _cloudinaryConfig.Value.CloudName,
+                _cloudinaryConfig.Value.ApiKey,
+                _cloudinaryConfig.Value.ApiSecret
+            );
+
+            _cloudinary = new Cloudinary(acc);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPhotoForUser(int userId, PhotoForCreationDto photoForCreationDto)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var userFromRepo = await _repo.GetUser(userId);
+            var file = photoForCreationDto.File;
+            var uploadResult = new ImageUploadResult();
+
+            // If file exists, read file into memory
+            if (file.Length > 0)
+            {
+                using (var stream = file.OpenReadStream())
+                {
+                    var uploadParams = new ImageUploadParams()
+                    {
+                        File = new FileDescription(file.Name, stream),
+                        Transformation = new Transformation().Width(500).Height(500).Crop("fill").Gravity("face")
+                    };
+
+                    uploadResult = _cloudinary.Upload(uploadParams);
+                }
+            }
+            photoForCreationDto.Url = uploadResult.Uri.ToString();
+            photoForCreationDto.PublicId = uploadResult.PublicId;
+
+            var photo = _mapper.Map<Photo>(photoForCreationDto);
+
+            // If use doesm't have a main photo yet, set it to true
+            if (!userFromRepo.Photos.Any(u => u.IsMain))
+                photo.IsMain = true;
+
+            userFromRepo.Photos.Add(photo);
+
+            if (await _repo.SaveAll())
+            {
+                // TODO: should be a CreatedAtRoute response
+                return Ok();
+            }
+
+            return BadRequest("Failed to add photo");
+        }
+    }
+}
