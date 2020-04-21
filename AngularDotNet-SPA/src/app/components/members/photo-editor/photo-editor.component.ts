03f1f9ae590a7599bdab5c90274af09856c96f28
Ryan Achten <ryan.achten@akqa.com>
2020-04-22 07:42:52 +1200

    Update photo uploader style and markup

--- AngularDotNet-SPA/src/app/components/members/photo-editor/photo-editor.component.ts
+++ AngularDotNet-SPA/src/app/components/members/photo-editor/photo-editor.component.ts
@@ -12,8 +12,7 @@ import { AuthService } from "src/app/services/auth.service";
 export class PhotoEditorComponent implements OnInit {
   @Input() photos: Photo[];
   uploader: FileUploader;
-  hasBaseDropZoneOver: boolean;
-  hasAnotherDropZoneOver: boolean;
+  hasBaseDropZoneOver: boolean = false;
   response: string;
   baseUrl = environment.apiUrl;
 
@@ -25,32 +24,28 @@ export class PhotoEditorComponent implements OnInit {
 
   initUploader() {
     this.uploader = new FileUploader({
-      url: `${this.baseUrl}users/${this.auth.decodedToken.nameId}/photos`,
+      url: `${this.baseUrl}users/${this.auth.decodedToken.nameid}/photos`,
       authToken: `Bearer ${localStorage.getItem("token")}`,
       isHTML5: true,
-      allowedFileType: ["images"],
       removeAfterUpload: true,
       autoUpload: false,
       maxFileSize: 10 * 1024 * 1024,
-      disableMultipart: true, // 'DisableMultipart' must be 'true' for formatDataFunction to be called.
-      formatDataFunctionIsAsync: true,
-      formatDataFunction: async (item) => {
-        return new Promise((resolve, reject) => {
-          resolve({
-            name: item._file.name,
-            length: item._file.size,
-            contentType: item._file.type,
-            date: new Date(),
-          });
-        });
-      },
     });
-    this.hasBaseDropZoneOver = false;
-    this.hasAnotherDropZoneOver = false;
 
-    this.response = "";
-
-    this.uploader.response.subscribe((res) => (this.response = res));
+    this.uploader.onAfterAddingFile = (file) => (file.withCredentials = false);
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const res: Photo = JSON.parse(response);
+        const photo = {
+          id: res.id,
+          url: res.url,
+          dateAdded: res.dateAdded,
+          description: res.description,
+          isMain: res.isMain,
+        };
+        this.photos.push(photo);
+      }
+    };
   }
 
   fileOverBase(e: any): void {
