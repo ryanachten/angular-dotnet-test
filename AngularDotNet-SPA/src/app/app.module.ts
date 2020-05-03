58b3530ce7e038050dd933215a310323b8ac0254
Ryan Achten <ryan.achten@akqa.com>
2020-05-04 08:07:53 +1200

    Add init refactor of register as reactive form

--- AngularDotNet-SPA/src/app/app.module.ts
+++ AngularDotNet-SPA/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
@@ -51,6 +51,7 @@ const tokenGetter = () => localStorage.getItem("token");
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     FileUploadModule,
     BrowserAnimationsModule,
     BsDropdownModule.forRoot(),
--- AngularDotNet-SPA/src/app/components/register/register.component.ts
+++ AngularDotNet-SPA/src/app/components/register/register.component.ts
@@ -1,25 +1,34 @@
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { AuthService } from "src/app/services/auth.service";
 import { AlertService } from "src/app/services/alert.service";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
   selector: "app-register",
   templateUrl: "./register.component.html",
-  styleUrls: ["./register.component.css"]
+  styleUrls: ["./register.component.css"],
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
 
   constructor(private auth: AuthService, private alert: AlertService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl(),
+    });
+  }
 
   register() {
-    this.auth.register(this.model).subscribe(
-      () => this.alert.success("Registered successfully"),
-      error => this.alert.error(error)
-    );
+    // this.auth.register(this.model).subscribe(
+    //   () => this.alert.success("Registered successfully"),
+    //   (error) => this.alert.error(error)
+    // );
+    console.log("registerForm", this.registerForm);
   }
 
   cancel() {
