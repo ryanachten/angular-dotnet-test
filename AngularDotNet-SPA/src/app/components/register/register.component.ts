b867161ae4ae29590c03e3a2724ee0f29028f313
Ryan Achten <ryan.achten@akqa.com>
2020-05-06 08:12:31 +1200

    Refactor register form using FormBuilder

--- AngularDotNet-SPA/src/app/components/register/register.component.ts
+++ AngularDotNet-SPA/src/app/components/register/register.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { AuthService } from "src/app/services/auth.service";
 import { AlertService } from "src/app/services/alert.service";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from "@angular/forms";
 
 @Component({
   selector: "app-register",
@@ -13,20 +18,33 @@ export class RegisterComponent implements OnInit {
   model: any = {};
   registerForm: FormGroup;
 
-  constructor(private auth: AuthService, private alert: AlertService) {}
+  constructor(
+    private auth: AuthService,
+    private alert: AlertService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit() {
-    this.registerForm = new FormGroup(
+    this.createRegisterForm();
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.fb.group(
       {
-        username: new FormControl("", Validators.required),
-        password: new FormControl("", [
-          Validators.required,
-          Validators.minLength(4),
-          Validators.maxLength(8),
-        ]),
-        confirmPassword: new FormControl("", Validators.required),
+        username: ["", Validators.required],
+        password: [
+          "",
+          [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(8),
+          ],
+        ],
+        confirmPassword: ["", Validators.required],
       },
-      this.passwordMatchValidator
+      {
+        validators: this.passwordMatchValidator,
+      }
     );
   }
 
