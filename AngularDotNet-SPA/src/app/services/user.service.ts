932abdd33b85dd37ecfcd67f1ef1dde9a358f5bf
Ryan Achten <ryan.achten@akqa.com>
2020-06-16 16:31:19 +1200

    Add paginatin model and apply to getUsers method

--- AngularDotNet-SPA/src/app/services/user.service.ts
+++ AngularDotNet-SPA/src/app/services/user.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@angular/core";
 import { environment } from "src/environments/environment";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { User } from "../models/user";
+import { PaginationResult } from "../models/Pagination";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root",
@@ -12,8 +14,33 @@ export class UserService {
 
   constructor(private http: HttpClient) {}
 
-  getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(`${this.baseUrl}users`);
+  getUsers(
+    page?: number,
+    itemsPerPage?: number
+  ): Observable<PaginationResult<User[]>> {
+    const paginatedResult: PaginationResult<User[]> = new PaginationResult<
+      User[]
+    >();
+
+    let params = new HttpParams();
+    if (page !== null && itemsPerPage !== null) {
+      params = params.append("pageNumber", page.toString());
+      params = params.append("pageSize", itemsPerPage.toString());
+    }
+
+    return this.http
+      .get<User[]>(`${this.baseUrl}users`, { observe: "response", params })
+      .pipe(
+        map((res) => {
+          paginatedResult.result = res.body;
+          if (res.headers.get("Pagination") !== null) {
+            paginatedResult.pagination = JSON.parse(
+              res.headers.get("Pagination")
+            );
+          }
+          return paginatedResult;
+        })
+      );
   }
 
   getUser(id: number): Observable<User> {
